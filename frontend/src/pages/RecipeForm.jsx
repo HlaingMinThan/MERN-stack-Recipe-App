d9442a90c5e6b793905e5e66a6d2d5f5da4f6aa8
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-01-10 16:36:56 +0630

    Ep-102-Add-Ingredient-one-by-one-Feature

--- frontend/src/pages/RecipeForm.jsx
+++ frontend/src/pages/RecipeForm.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import plus from '../assets/plus.svg';
 import Ingredients from '../components/Ingredients';
 
 export default function RecipeForm() {
+    let [ingredients, setIngredients] = useState([]);
+    let [newIngredient, setNewIngredient] = useState('');
+
+    let addIngredient = () => {
+        setIngredients(prev => [newIngredient, ...prev])
+        setNewIngredient('');
+    }
     return (
         <div className="mx-auto max-w-md border-2 border-white p-4">
             <h1 className="mb-6 text-2xl font-bold text-orange-400 text-center">Recipe Create Form</h1>
@@ -9,11 +17,11 @@ export default function RecipeForm() {
                 <input type="text" placeholder="Recipe Title" className="w-full p-1" />
                 <textarea placeholder="Recipe Description" rows="5" className="w-full p-1" />
                 <div className="flex space-x-2 items-center">
-                    <input type="text" placeholder="Recipe Ingredient" className="w-full p-1" />
-                    <img src={plus} alt="" className='cursor-pointer' />
+                    <input value={newIngredient} onChange={e => setNewIngredient(e.target.value)} type="text" placeholder="Recipe Ingredient" className="w-full p-1" />
+                    <img src={plus} alt="" className='cursor-pointer' onClick={addIngredient} />
                 </div>
                 <div>
-                    <Ingredients ingredients={['3 eggs', 'water']} />
+                    <Ingredients ingredients={ingredients} />
                 </div>
                 <button className='w-full px-3 py-1 rounded-full bg-orange-400 text-white'>Create Recipe</button>
             </form>
