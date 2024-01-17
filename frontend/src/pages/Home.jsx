747df49a8a199fe5b93c229f416b8c58387138c9
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-01-17 13:25:52 +0630

    Ep-105-Pagination-Frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,43 @@
 import { useEffect, useState } from "react"
 import RecipeCard from '../components/RecipeCard';
+import Pagination from '../components/Pagination';
+import { useLocation } from 'react-router-dom';
 
 export default function Home() {
 
     let [recipes, setRecipes] = useState([]);
 
+    let location = useLocation();
+    let searchQuery = new URLSearchParams(location.search);
+    let page = searchQuery.get('page');
+
+
     useEffect(() => {
         let fetchRecipes = async () => {
-            let response = await fetch('http://localhost:8000/api/recipes');
+            let response = await fetch('http://localhost:8000/api/recipes?page=' + page);
             if (response.ok) {
                 let data = await response.json();
                 setRecipes(data);
+
+                //scroll to top
+                window.scroll({ top: 0, left: 0, behavior: "smooth" })
             }
         }
 
         fetchRecipes();
-    }, [])
+    }, [page])
+
+    //backend info (hardcode)
+    let links = {
+        nextPage: true,
+        previousPage: false,
+        currentPage: 1,//hardcode
+        loopableLinks: [
+            { number: 1 },
+            { number: 2 },
+            { number: 3 },
+        ]
+    };
 
     return (
         <div className="space-y-3">
@@ -23,7 +45,7 @@ export default function Home() {
                 <RecipeCard recipe={recipe} key={recipe._id} />
             ))
             )}
-
+            <Pagination links={links} page={page} />
         </div>
     )
 }
