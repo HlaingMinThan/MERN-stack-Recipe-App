1f4491d74f290b8044552bb240f6132625623a75
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-02-07 16:15:06 +0630

    Ep-110-Register-user-api

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,11 +1,29 @@
-import User from '../models/User';
-
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const UserController = {
     login : (req,res) => {
         return res.json({msg : 'user login api hit'});
     },
-    register : (req,res) => {
-        return res.json({msg : 'user register api hit'});
+    register : async (req,res) => {
+        try {
+            let {name,email,password} = req.body;
+            let userExists = await User.findOne({email});
+            if(userExists) {
+                throw new Error('user already exists');
+            }
+            
+            let salt = await bcrypt.genSalt();
+            let hashValue = await bcrypt.hash(password,salt);
+
+            let user = await User.create({
+                name,
+                email,
+                password : hashValue
+            });
+            return res.json(user);
+        }catch(e) {
+            return res.status(400).json({error : e.message});
+        }
     }
 }
 
