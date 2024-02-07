e6672787cfed4dead1154e03d0719caaf851b02a
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-02-07 16:48:04 +0630

    Ep-111-refactor-custom-method-in-model

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+
 const UserController = {
     login : (req,res) => {
         return res.json({msg : 'user login api hit'});
@@ -7,19 +7,7 @@ const UserController = {
     register : async (req,res) => {
         try {
             let {name,email,password} = req.body;
-            let userExists = await User.findOne({email});
-            if(userExists) {
-                throw new Error('user already exists');
-            }
-            
-            let salt = await bcrypt.genSalt();
-            let hashValue = await bcrypt.hash(password,salt);
-
-            let user = await User.create({
-                name,
-                email,
-                password : hashValue
-            });
+            let user = await User.register(name,email,password);
             return res.json(user);
         }catch(e) {
             return res.status(400).json({error : e.message});
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -17,5 +18,22 @@ const UserSchema = new Schema({
         required : true,
     },
 });
+
+UserSchema.statics.register = async function(name,email,password) {
+    let userExists = await this.findOne({email});
+    if(userExists) {
+        throw new Error('user already exists');
+    }
+    
+    let salt = await bcrypt.genSalt();
+    let hashValue = await bcrypt.hash(password,salt);
+
+    let user = await this.create({
+        name,
+        email,
+        password : hashValue
+    });
+    return user;
+}
 
 module.exports = mongoose.model('User',UserSchema);
